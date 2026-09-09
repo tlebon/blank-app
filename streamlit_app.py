500f7ea9a95069a90f8f7806c3d83ad2becc554d
Timothy LeBon <tlebon@gmail.com>
2026-09-09 12:57:10 +0000

    french fries

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,30 +6,60 @@ st.write('wassup folks. my name is tim and im a very complicated person with man
 tab1, tab2, tab3 = st.tabs(["get to know me", "get to know the world", "Owl"])
 
 with tab1:
-  option = st.selectbox(label='which thing is my favorite?', options=[
-                        'cat', 'dog', 'lizard', 'french fries'], placeholder='choose one!', index=None)
-  if st.button('Make a choice'):
-      if option == 'dog':
-          st.balloons()
-          st.success(
-              'Yes its a dog, but french fries are a close second!', icon="✅")
-          st.image(image='Oskar.jpeg')
-      elif option =='french fries':
-          st.error('ooh i do love fries, but not number 1 🍟')
-      else:
-          st.error('Do you know me at all?', icon="😩")
-with tab2:
-    col1, col2 = st.columns(spec = [1,1])
-    with col1:
-        st.header('What year was gandhi born?')
-        year = st.select_slider(options=[a for a in range(1860, 1980)], label= 'choose a year')
-        if st.button('gandhi bday party'):
-          if year == '1869':
+    option = st.selectbox(label='which thing is my favorite?', options=[
+                          'cat', 'dog', 'lizard', 'french fries'], placeholder='choose one!', index=None)
+    if st.button('Make a choice'):
+        if option == 'dog':
             st.balloons()
-            st.success('wtf... you cheated? how did you know that? theres no way')
-          else: 
-            st.error('yea how would you guess that, its impossible.. ok its closer to the beginning and divisible by 3')
+            st.success(
+                'Yes its a dog, but french fries are a close second!', icon="✅")
+            st.image(image='Oskar.jpeg')
+        elif option == 'french fries':
+            st.error('ooh i do love fries, but not number 1 🍟')
+        else:
+            st.error('Do you know me at all?', icon="😩")
+with tab2:
+    col1, col2 = st.columns(spec=[1, 1])
+    with col1:
+        st.header(
+            'How many french fries (thick cut) is recommended for a single serving?')
+        serving = st.select_slider(
+            options=[a*5 for a in range(0, 15)], label='choose a serving')
+        if st.button('fries'):
+            if serving == 20:
+                st.success(
+                    'yummy', icon="🍟")
+            elif serving < 20:
+                st.error(
+                    'im still hungry', icon="🤤")
+            elif serving > 20:
+                st.error(
+                    'i feel gross', icon="🫃")
 
-    with col2: 
-       st.header('how many european cities have i been to')
-       
+    with col2:
+        st.header('which french fries types are not real')
+        option_map = {
+            0: "curly",
+            1: "thin",
+            2: "thick cut",
+            3: "crinkle",
+            4: "waffle",
+            5: "tornado",
+            6: "wedge",
+            7: "croquette",
+            8: "popsicle"
+        }
+        selection = st.segmented_control(
+            "Tool",
+            options=option_map.keys(),
+            format_func=lambda option: option_map[option],
+            selection_mode="multi",
+        )
+
+        if st.button('lets eat!'):
+            if selection == [8]:
+                st.snow()
+                st.success(
+                    'hey, not real yet anyway, but i can dream', icon='☃️')
+            else: 
+                st.error('i had one of those earlier today')
