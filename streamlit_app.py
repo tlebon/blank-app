885e88e55dd0cd0bb76c0d7f49e4f0c9ae53d3b9
Timothy LeBon <tlebon@gmail.com>
2026-09-09 10:04:27 +0000

    placeholder

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,12 @@ import streamlit as st
 
 st.header("I'm tim 😊,")
 st.write('wassup folks. my name is tim and im a very complicated person with many wants and desires.')
-option = st.selectbox(label='which thing is my favorite?', options=['cat','dog','lizard','french fries'])
+option = st.selectbox(label='which thing is my favorite?', options=[
+                      'cat', 'dog', 'lizard', 'french fries'], placeholder='choose one!', index=None)
 if st.button('Make a choice'):
-  if option=='dog':
-    st.balloons()
-    st.success('Yes its a dog, but french fries are a close second!', icon="✅")
-  else: 
-    st.error('Do you know me at all?', icon="😩")
-
-
-
+    if option == 'dog':
+        st.balloons()
+        st.success(
+            'Yes its a dog, but french fries are a close second!', icon="✅")
+    else:
+        st.error('Do you know me at all?', icon="😩")
